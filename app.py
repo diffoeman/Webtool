4ac1789693a47297b269ac4eec356b5c2dc381c6
Dave Jonk <davie.jonk@gmail.com>
2025-01-14 16:39:35 +0100

    Fix indentatie fout in app.py

--- app.py
+++ app.py
@@ -35,8 +35,7 @@ def upload_files():
     return jsonify({'activities': all_data})  # Stuur de data terug naar de frontend
 
 if __name__ == '__main__':
-import os
-
-port = int(os.environ.get('PORT', 5000))  # Gebruik de poort van Render, standaard 5000
-app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Gebruik de poort van Render, standaard 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
 
